bfdb4b993cb500e9db5c26a27a2609ec5f3f9886
Sara Rioseco <sara.rioseco@gmail.com>
2024-08-14 10:53:56 -0400

    Add modal reusable component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,24 @@
+import styles from './style.module.css';
+
+type ModalProps = {
+  children: string | JSX.Element | JSX.Element[];
+  onClose?: () => void;
+};
+
+export default function Modal({ children, onClose }: ModalProps) {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.wrapper}>
+        {onClose && (
+          <span
+            className={styles.closeButton}
+            onClick={onClose}
+          >
+            &times;
+          </span>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,13 +4,8 @@ import Input from '../../components/Input';
 import { useLogin } from './useLogin';
 
 export function Login() {
-
-  const {
-    formData,
-    handleFieldChange,
-    errorLabel,
-    handleLoginClick,
-  } = useLogin();
+  const { formData, handleFieldChange, errorLabel, handleLoginClick } =
+    useLogin();
 
   return (
     <main className={styles.login}>
@@ -22,14 +17,14 @@ export function Login() {
           type={'email'}
           placeholder=' '
           value={formData.email}
-          handleOnChange={(e) => handleFieldChange('email', e)}
+          handleOnChange={e => handleFieldChange('email', e)}
         />
         <Input
           label={'Enter your password'}
           type={'password'}
           placeholder=' '
           value={formData.password}
-          handleOnChange={(e) => handleFieldChange('password', e)}
+          handleOnChange={e => handleFieldChange('password', e)}
         />
         <label className={styles.labelErrorLogin}>{errorLabel}</label>
         <Button

88f77f627c56047cde623b3997cbcb9c37717087
saralopezbz <saralopezbz@gmail.com>
2024-08-16 14:31:53 -0400

    Move user registration form from Home to DashboardAdmin and refactor modal handling

--- src/components/DashboardAdmin/index.tsx
+++ src/components/DashboardAdmin/index.tsx
@@ -1,59 +1,64 @@
 import { useEffect, useState } from 'react';
 import { Button, Input, Loader, UserList } from '../../components';
+import RegisterModal from '../../components/Modal/RegisterModal';
 import { useDashborad } from '../../hooks';
 import styles from './style.module.css';
 
 export function DashboardAdmin() {
+  const { searchTerm, users, handleSearchClick, getAllUsers } = useDashborad();
 
-    const {
-        searchTerm,
-        users,
-        handleSearchClick,
-        getAllUsers
-    } = useDashborad();
+  const [loading, setLoading] = useState(true);
+  const [word, setWord] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar el modal
 
-    const [loading, setLoading] = useState(true);
-    const [word, setWord] = useState<string>('');
+  useEffect(() => {
+    getAllUsers(searchTerm);
+    setLoading(false);
+  }, [searchTerm]);
 
-    useEffect(() => {
-            getAllUsers(searchTerm)
-            setLoading(false)
-    }, [searchTerm])
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    return (
-        <div className={styles.content}>
-            <h1>Dashboard Admin</h1>
-            <form className={styles.form}>
-                <Input
-                    label={'Enter name, surname, email or country'}
-                    type={'text'}
-                    placeholder=' '
-                    value={word}
-                    handleOnChange={e => setWord(e.target.value)}
-                />
-                <Button
-                    label={'Search'}
-                    onClick={() => handleSearchClick(word)}
-                    type='button'
-                />
-            </form>
-            <div className={styles.listHeader}>
-                <h2>User List</h2>
-                <Button
-                    label={'Add User'}
-                    onClick={() => handleSearchClick(word)}
-                    type='button'
-                />
-            </div>
-            {
-                loading ? (
-                    <div className={styles.loader}>
-                        <Loader />
-                    </div>
-                ) : (
-                    <UserList users={users} />
-                )
-            }
-        </ div>
-    )
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className={styles.content}>
+      <h1>Dashboard Admin</h1>
+      <form className={styles.form}>
+        <Input
+          label={'Enter name, surname, email or country'}
+          type={'text'}
+          placeholder=" "
+          value={word}
+          handleOnChange={(e) => setWord(e.target.value)}
+        />
+        <Button
+          label={'Search'}
+          onClick={() => handleSearchClick(word)}
+          type="button"
+        />
+      </form>
+      <div className={styles.listHeader}>
+        <h2>User List</h2>
+        <Button
+          label={'Add User'}
+          onClick={openModal} // Abre el modal al hacer clic en "Add User"
+          type="button"
+        />
+      </div>
+      {loading ? (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      ) : (
+        <UserList users={users} />
+      )}
+
+      {/* Renderiza el modal si isModalOpen es true */}
+      {isModalOpen && <RegisterModal onClose={closeModal} />}
+    </div>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,10 @@
 import { Header } from '../../components';
-import { useState } from 'react';
 import styles from './style.module.css';
-import RegisterModal from '../../components/Modal/RegisterModal';
 
 export function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const userName = sessionStorage.getItem('userName')?.toLocaleString();
   const userSurname = sessionStorage.getItem('userSurname')?.toLocaleString();
   const userRole = sessionStorage.getItem('userRole')?.toLocaleString();
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <>
@@ -24,14 +13,6 @@ export function Home() {
         <h1>This is the home view</h1>
         <h2>{`Hello ${userName} ${userSurname}`}</h2>
         <h2>{`Your role is ${userRole}`}</h2>
-
-        {/* Button to open the modal */}
-        <button onClick={openModal} className={styles.openModalButton}>
-          Register New User
-        </button>
-
-        {/* Render the modal if isModalOpen is true*/}
-        {isModalOpen && <RegisterModal onClose={closeModal} />}
       </main>
     </>
   );

a82445305616f1d43b7a970dfadaaa6d37ee0d91
Silvana Vitali <silgivia@hotmail.com>
2024-08-16 18:03:30 -0400

    refactor: useDashboard

--- src/hooks/useDashboard.ts
+++ src/hooks/useDashboard.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { User } from '../interfaces/User';
+import { userServices } from './../services'
 
 export function useDashborad() {
 
@@ -11,24 +12,11 @@ export function useDashborad() {
     const [users, setUsers] = useState<User[]>([]);
 
     const getAllUsers = async (searchTerm: string) => {
-
-        const baseUrl = `${import.meta.env.VITE_BACKEND_URL}/${import.meta.env.VITE_DASHBOARD_ADMIN_URL}`;
-        const url = searchTerm ? `${baseUrl}?search=${encodeURIComponent(searchTerm)}` : baseUrl;
-
         try {
             const token = sessionStorage.getItem('accessToken')
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                    'Authorization': `Bearer ${token}`
-                },
-            })
-
-            if (!response.ok) throw Error('Error fetching')
+            const response = await userServices.getAll(token!, searchTerm)
             const data = await response.json();
             setUsers(data)
-
         } catch (error) {
             console.log(error);
         }

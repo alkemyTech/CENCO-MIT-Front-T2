4a8140a3d14feb03a3a039f0764f0e9ded6d9c23
Silvana Vitali <silgivia@hotmail.com>
2024-08-16 23:11:05 -0400

    feat: implement responsive layout

--- src/pages/NotFoundPage/index..tsx
+++ src/pages/NotFoundPage/index..tsx
@@ -1,10 +1,19 @@
 import styles from './style.module.css';
-import image from '../../assets/img/pageNotFound.png';
+import img404 from '../../assets/img/img404.png';
+import error404 from '../../assets/img/notFound.png';
+import { Button } from '../../components';
 
 export function NotFoundPage() {
   return (
-    <main className={styles.notFound}>
-      <img src={image} alt='avatar' />
+    <main className={styles.content}>
+      <div className={styles.notFound}>
+        <img className={styles.err} src={error404} alt='error404' />
+        <img className={styles.img} src={img404} alt='image404' />
+      </div>
+      <Button
+        label='GO BACK HOME'
+        onClick={() => { <a href="/"></a> }}
+      />
     </main>
   )
 }

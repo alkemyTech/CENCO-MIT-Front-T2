d810d2828c3c20ee2fd1f5f64e7efcc32c641955
Sara Rioseco <sara.rioseco@gmail.com>
2024-08-19 16:37:06 -0400

    Add update password feature

--- src/validations/isTokenValid.ts
+++
@@ -1,6 +0,0 @@
-export const isTokenExpired = (token: string) => {
-  const { exp } = JSON.parse(
-    decodeURIComponent(escape(atob(token.split('.')[1])))
-  );
-  return exp * 1000 < new Date().getTime();
-}
